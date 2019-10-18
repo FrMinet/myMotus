5ea925dfd0f517afa65d3dbd7253402c271baf19
FrMinet <fr.minet@gmail.com>
2019-10-18 15:03:08 +0200

    Version 0.0.2 // Basis application in php :)

--- index.php
+++ index.php
@@ -1,47 +1,63 @@
 <?php
 
 // VARIABLES
-$word_to_find = array("e","s","c","a","b","e","a","u");
+$word_to_find = "maison";
+$limit_char = strlen($word_to_find);
+$splitted_word_to_find = array();
 $splitted_answer = array();
+$corrective = array();
 $turn_counter = 0;
 $continue = true;
-$has_win = false;
-
-// FONCTIONS
-function check_answer($user_answer)
-{
-    
-}
 
 echo("Bienvenue dans 'MyMotus', le jeu où vous devez retrouver un mot de 8 lettres" . "\n");
 // BOUCLE DE JEU
-do {
+while ($continue == true) {
     echo("Nombre de tours : " . $turn_counter . "\n");
     $user_answer = readline("Quelle est votre proposition ? : ");
-    // $user_answer = strtolower($user_answer);
-    // $user_answer = str_replace(" ", "", $user_answer);
-    if ($user_answer = "quit")
+    $user_answer = strtolower($user_answer);
+    $user_answer = str_replace(" ", "", $user_answer);
+    
+    if ($user_answer == "quit")
     {
         $continue = false;
         $message = "Merci d'avoir participé !";
     }
-    elseif (strlen($user_answer) != 8)
+    elseif (strlen($user_answer) != $limit_char)
     {
-        $message = "ERREUR : Votre proposition doit être de 8 caractères.";
+        $message = "ERREUR : Votre proposition doit être de " . $limit_char . " caractères.";
         $turn_counter++;
     }
     else
     {
+        $splitted_word_to_find = preg_split('//', $word_to_find, -1, PREG_SPLIT_NO_EMPTY);
         $splitted_answer = preg_split('//', $user_answer, -1, PREG_SPLIT_NO_EMPTY);
-        for ($i = 0; $i <= 7; $i++)
+        for ($i = 0; $i <= ($limit_char - 1); $i++)
         {
-            if ($splitted_answer[$i] = $word_to_find[$i])
+            if ($splitted_answer[$i] == $splitted_word_to_find[$i])
             {
-                $corrective .= strtoupper($splitted_answer[$i]);
-                return $corrective; 
+                $corrective[$i] = strtoupper($splitted_answer[$i]);
             }
+            else
+            {
+                if (strpos($word_to_find, $splitted_answer[$i]))
+                {
+                    $corrective[$i] = $splitted_answer[$i];
+                }
+                else
+                {
+                    $corrective[$i] = "*";
+                }
+            }    
         }
+        $message = implode($corrective);
+        $turn_counter++;
     }
-    echo $message;
-} while ($continue = false);
+    echo($message . "\n");
+    if (strtolower($message) == $word_to_find)
+    {
+        echo("Félicitations vous avez gagné! \n");
+        $continue = false;
+    }
+}
 
+?>
